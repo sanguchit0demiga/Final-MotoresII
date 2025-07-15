3a6884f1bc1b4adef69587bc41438e8196e720b4
sanguchit0demiga <camilarocio005@gmail.com>
2025-07-15 20:19:52 -0300

    map
    lo sigo haciendo

---
+++ Assets/scripts/CameraLookAt.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSensitivity = 100f;
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        xRotation = transform.localEulerAngles.x;
+        if (xRotation > 180f)
+            xRotation -= 360f;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
